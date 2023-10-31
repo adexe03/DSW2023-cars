e33b1fd5944570a699a974b862fda2fdfceb2c03
Adexe Reyes <adexe.reyes.cb@gmail.com>
2023-10-31 20:12:34 +0000

    read JSON with cars

--- src/controllers/CarController.php
+++ src/controllers/CarController.php
@@ -5,9 +5,14 @@ class CarController
 
   public function __construct()
   {
-    $this->cars[] = new Car("2453", "Toyota", "Supra", 2023, "white");
-    $this->cars[] = new Car("4687", "Toyota", "AE86", 1998, "black");
-    $this->cars[] = new Car("1231", "Nissan", "Silvia S15", 1999, "red");
+    // $this->cars[] = new Car("2453", "Toyota", "Supra", 2023, "white");
+    // $this->cars[] = new Car("4687", "Toyota", "AE86", 1998, "black");
+    // $this->cars[] = new Car("1231", "Nissan", "Silvia S15", 1999, "red");
+    $json = file_get_contents('../data/cars.json');
+    $carsJSON = json_decode($json);
+    foreach ($carsJSON as $carJSON) {
+      $this->cars[] = new Car($carJSON->id, $carJSON->make, $carJSON->model, $carJSON->year, $carJSON->color);
+    }
   }
 
   public function list()
